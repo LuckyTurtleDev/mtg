be675b332829f645505bf17a82a4791e9efd39c8
luckyturtledev <git@lukas1818.de>
2023-12-11 20:54:58 +0100

    start memory cache

--- src/cache.rs
+++ src/cache.rs
@@ -4,7 +4,14 @@ use iced::Command;
 use log::{error, info};
 use once_cell::sync::Lazy;
 use reqwest::Url;
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+	borrow::Borrow,
+	collections::{BTreeMap, HashMap},
+	hash::Hash,
+	mem::replace,
+	path::PathBuf,
+	sync::Arc
+};
 use tokio::fs;
 
 pub const URL_CACHE: Lazy<PathBuf> = Lazy::new(|| DIRS.cache_dir().join("img"));
@@ -16,6 +23,7 @@ enum CacheState {
 }
 
 #[derive(Debug, Default)]
+///Chache content from a Url to a file
 pub struct UrlCacher(HashMap<Url, CacheState>);
 
 impl UrlCacher {
@@ -90,4 +98,83 @@ fn url_to_file(url: &Url) -> PathBuf {
 		path.set_extension(extension);
 	}
 	path
+}
+
+#[derive(Debug)]
+/// keep data in memory.
+/// If full the oldest data will be removed.
+/// If a cache miss appears, the data will be loaded to memory again.
+pub struct Cacher<K, V>
+where
+	K: Eq + Hash
+{
+	/// maps the key to (last_acess_time, value)
+	data: HashMap<Arc<K>, (u32, V)>,
+	/// maps the last acess time to the key used in the HashMap
+	last_acess: BTreeMap<u32, Arc<K>>,
+	time: u32,
+	max_size: usize
+}
+
+impl<K, V> Default for Cacher<K, V>
+where
+	K: Eq + Hash
+{
+	fn default() -> Self {
+		Cacher {
+			data: Default::default(),
+			last_acess: Default::default(),
+			time: 0,
+			max_size: 1000
+		}
+	}
+}
+
+impl<K, V> Cacher<K, V>
+where
+	K: Eq + Hash
+{
+	/// get key from cache if present
+	fn get<Q>(&self, key: &Q) -> Option<&V>
+	where
+		Arc<K>: Borrow<Q>,
+		Q: Hash + Eq + ?Sized
+	{
+		self.data.get(key).map(|(_, k)| k)
+	}
+
+	/// Load missing data to cache and reseltt acess time.
+	/// Should be called before `get()`
+	fn need_soon<Q>(&mut self, key: &Q)
+	where
+		Arc<K>: Borrow<Q>,
+		Q: Hash + Eq + ?Sized,
+		Q: ToOwned<Owned = K>
+	{
+		let time = self.data.get_mut(key).map(|(time, _value)| time);
+		match time {
+			Some(time) => {
+				// Reset last acess time
+				let value = self.last_acess.remove(time);
+				self.last_acess
+					.insert(self.time, value.unwrap_or_else(|| Arc::new(key.to_owned())));
+				let _ = replace(time, self.time);
+				self.time += 1;
+			},
+			None => {
+				// TODO: create command to load missing cache
+				todo!()
+			}
+		}
+	}
+
+	fn cache_replacement(&mut self) {
+		while self.last_acess.len() > self.max_size {
+			// remove oldest element from cache
+			let key = self.last_acess.pop_first().map(|(_i, key)| key);
+			if let Some(key) = key {
+				self.data.remove(&key);
+			}
+		}
+	}
 }
